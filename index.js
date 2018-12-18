04b3a9acd432afa4f77fecc479989fad2f9e1d08
Julian M. Qapo <julian.qapo@gmail.com>
2018-12-18 21:37:49 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,26 +1,3 @@
-// function BoardMember(name, homeState, training){
-//   this.name = name
-//   this.homeState = homeState
-//   this.training = training
-//
-//   this.veto = function(){
-//     return 'No, I must disagree'
-//   }
-//   this.approve = function(){
-//     return 'You can do that!'
-//   }
-//   this.doCharity = function(){
-//     return "I like to help people."
-//   }
-//
-//   this.releasePressStatement = function(){
-//     return "You will see great things from Scuber."
-//   }
-//
-//   this.sayHi = function(){
-//     return `Hi, my name is ${this.name}. I am from ${this.homeState}, and I was trained in ${this.training}.`
-//   }
-// }
 
 function BoardMember(name, homeState, training){
   this.name = name;
@@ -51,4 +28,4 @@ BoardMember.prototype.releasePressStatement = function(){
 BoardMember.prototype.sayHi = function(){
     return `Hi, my name is ${this.name}. I am from ${this.homeState}, and I was trained in ${this.training}.`
   }
-}
+
